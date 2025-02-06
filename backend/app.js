535e2f0d4c567d7bc1fdd262034d4fa7a5003d0e
greyphoenix1 <rahmanmukit905@gmail.com>
2025-02-06 11:43:22 -0500

    path fix

--- backend/app.js
+++ backend/app.js
@@ -53,7 +53,7 @@ app.get('/api/french', (req, res) => {
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'assets', 'index.html'));
 })
 
 
