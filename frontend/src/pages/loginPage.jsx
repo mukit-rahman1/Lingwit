ace8b100c1a7cc8df7f33cd6e7226f7081e63397
greyphoenix1 <rahmanmukit905@gmail.com>
2025-02-20 19:01:47 -0500

    Added status msg if site is loading

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -8,9 +8,11 @@ function LoginPage() {
     const [password, setPassword] = useState("");
     const [status, setStatus] = useState("");
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
     const handleLogin = async () => {
         try {
+            setLoading(true);
             const response = await axios.post('https://lingwit-backend.onrender.com/api/auth/login', {
                 username,
                 password,
@@ -24,12 +26,18 @@ function LoginPage() {
             console.log(error);
             console.log("login error: ", error.response ? error.response.data : error.message)
             setStatus("Incorrect Username or Password!");
+            setLoading(false);
         }
     }
 
     return(<>
     <HeaderTop />
     <div className="auth-container">
+        {loading &&
+            <div className="absolute top-[10%] left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-3xl border-black border-2">
+                <a className="font-mono">Signing in. May take 20-50 seconds. Please wait...."</a>
+            </div>
+        }
         <input placeholder="Username" className="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
         <input placeholder="Password" className="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
         <a className="absolute bottom-1/3 -translate-x-1/2 -translate-y-1/2 font-mono">{status}</a>
--- frontend/src/pages/registerPage.jsx
+++ frontend/src/pages/registerPage.jsx
@@ -8,9 +8,11 @@ function RegisterPage() {
     const [password, setPassword] = useState("");
     const [status, setStatus] = useState("");
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
     const handleLogin = async () => {
         try {
+            setLoading(true);
             const response = await axios.post('https://lingwit-backend.onrender.com/api/auth/register', {
                 username,
                 password
@@ -24,12 +26,19 @@ function RegisterPage() {
             console.log(error.response ? error.response : error.data);
             console.log("register error: ", error.response ? error.response.data : error.message)
             setStatus("Username already exists");
+            setLoading(false);
         }
     }
+
 
     return(<>
     <HeaderTop />
     <div className="auth-container">
+        { loading &&
+            <div className="absolute top-[10%] left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-3xl border-black border-2">
+                <a className="font-mono">Signing in. May take 20-50 seconds. Please wait...."</a>
+            </div>
+        }
         <input placeholder="Username" className="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
         <input placeholder="Password" className="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
         
